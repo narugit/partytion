434cc5ee3467878cfb2d6d7cd1dc8a28c165c3f5
hiyoko3 <sb.gm.0823@gmail.com>
2019-07-23 23:51:01 +0900

    Add ViewControllers

---
+++ partytion-app/partytion-app/presentation/SixthViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SixthViewController.swift
+//  partytion-app
+//
+//  Created by tatsuya_oyanagi on 2019/07/23.
+//  Copyright © 2019 naruhiyo. All rights reserved.
+//
+
+import UIKit
+
+class SixthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
