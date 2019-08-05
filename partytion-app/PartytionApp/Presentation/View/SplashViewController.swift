67c7896f5e1ab00ce21d00980ee1868db9d8de5f
hiyoko3 <sb.gm.0823@gmail.com>
2019-08-05 20:28:01 +0900

    Add sample storyboard and controller

---
+++ partytion-app/PartytionApp/Presentation/View/SplashViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  PartytionApp
+//
+//  Created by tatsuya_oyanagi on 2019/08/05.
+//  Copyright © 2019 naruhiyo. All rights reserved.
+//
+
+import UIKit
+
+
+class SplashViewController: UIViewController {
+    var logoImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.blue
+        
+        //imageView作成
+//        self.logoImageView = UIImageView(frame: CGRectMake(0, 0, 204, 77))
+//        //画面centerに
+//        self.logoImageView.center = self.view.center
+//        //logo設定
+//        self.logoImageView.image = UIImage(named: "logo")
+//        //viewに追加
+//        self.view.addSubview(self.logoImageView)
+        
+    }
+}
--- partytion-app/PartytionApp/Presentation/View/ViewController.swift
+++ partytion-app/PartytionApp/Presentation/View/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var labeltest :UILabel!
+    @IBOutlet var buttonTest :UIButton!
+    var count = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func sampleButtonTapped(_ sender : Any) {
+        count += 1
+        if(count%2 == 0){
+            labeltest.text = "Swift Test"
+        }
+        else{
+            labeltest?.text = "tapped ! hogehoge"
+        }
+    }
 }
 
