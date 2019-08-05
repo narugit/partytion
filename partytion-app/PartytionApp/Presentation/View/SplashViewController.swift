f17ca8a15059f3726b38b71ec692eb64ae556e0a
hiyoko3 <sb.gm.0823@gmail.com>
2019-08-05 21:18:52 +0900

    Add sample gif

--- partytion-app/PartytionApp/Presentation/View/SplashViewController.swift
+++ partytion-app/PartytionApp/Presentation/View/SplashViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Lottie
 
 
 class SplashViewController: UIViewController {
@@ -17,6 +18,7 @@ class SplashViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         self.view.backgroundColor = UIColor.blue
         
+        showAnimation()
         //imageView作成
 //        self.logoImageView = UIImageView(frame: CGRectMake(0, 0, 204, 77))
 //        //画面centerに
@@ -27,4 +29,17 @@ class SplashViewController: UIViewController {
 //        self.view.addSubview(self.logoImageView)
         
     }
+    
+    func showAnimation() {
+        let animationView = AnimationView(name: "BeerBubbles")
+        animationView.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height)
+        animationView.center = self.view.center
+        animationView.contentMode = .scaleAspectFit
+        animationView.animationSpeed = 1
+        
+        view.addSubview(animationView)
+        
+        animationView.play()
+    }
+
 }
