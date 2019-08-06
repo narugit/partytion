ce4ef68c8ee2c21b9f884b003cb2aaefc8002cfa
narugit <narusens@gmail.com>
2019-08-06 23:38:05 +0900

    Add player screen

--- partytion-app/PartytionApp/Presentation/View/PlayerViewController.swift
+++ partytion-app/PartytionApp/Presentation/View/PlayerViewController.swift
@@ -9,9 +9,28 @@
 import UIKit
 
 class PlayerViewController: UIViewController {
+    @IBOutlet var inputPlayerNumber: UITextField!
+    @IBOutlet var nextButton: UIButton!
+    
     // プログラムの読み込みが完了
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.inputPlayerNumber.keyboardType = UIKeyboardType.numberPad
+    }
+    
+    @IBAction func nextButtonTapped(_ sender: Any) {
+        self.moveQuestionScreen()
+    }
+    // 質問作成画面への移行
+    private func moveQuestionScreen() {
+        let questionStoryboard :UIStoryboard = UIStoryboard(name: "QuestionScreen", bundle: nil)
+        let questionScreen :UIViewController = questionStoryboard.instantiateViewController(withIdentifier: "QuestionViewController")
+        
+        if let secondVC = questionScreen as? QuestionViewController {
+            secondVC.playerNumber = inputPlayerNumber.text
+        }
+        
+        present(questionScreen, animated: true, completion: nil)
     }
     
 }
