ffd226911b136f06bf9d2bf2e76bff3ccddc2171
hiyoko3 <sb.gm.0823@gmail.com>
2019-08-07 21:37:55 +0900

    Remove `xcuserdata`

--- partytion-app/PartytionApp/Presentation/View/QuestionViewController.swift
+++ partytion-app/PartytionApp/Presentation/View/QuestionViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class QuestionViewController: UIViewController {
     @IBOutlet var testText: UILabel!
+    @IBOutlet var nextButton: UIButton!
     var playerNumber: String!
     
     // プログラムの読み込みが完了
@@ -16,4 +17,17 @@ class QuestionViewController: UIViewController {
         super.viewDidLoad()
 //        testText.text = self.playerNumber
     }
+    
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+        self.moveToAnswerScreen()
+    }
+
+    // 質問作成画面への移行
+    private func moveToAnswerScreen() {
+        let answerStoryboard :UIStoryboard = UIStoryboard(name: "AnswerScreen", bundle: nil)
+        let answerViewController :UIViewController = answerStoryboard.instantiateViewController(withIdentifier: "AnswerViewController")
+        
+        present(answerViewController, animated: true, completion: nil)
+    }
+    
 }
